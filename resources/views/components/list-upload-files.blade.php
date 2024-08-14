5bcbebebc1de9c4c1bbb2993a4574979b0103a38
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-08-14 12:23:59 +0300

    feat: add template upload files modal

---
+++ resources/views/components/list-upload-files.blade.php
@@ -0,0 +1,16 @@
+<div id="modal-upload" class="d-flex flex-row align-items-center d-none">
+    <div id="upload-block" class="d-flex flex-column w-100 bg-white p-3 rounded-4">
+        <div class="d-flex flex-row w-100 mb-3 align-items-center">
+            <div class="w-100 d-flex align-items-end justify-content-end">
+                <button class="btn px-0 py-0 text-danger" type="button" data-bs-dismiss="modal" aria-label="Close">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div id="upload-items" class="overflow-y-auto" style="max-height: 425px; max-width: 100%">
+
+        </div>
+    </div>
+</div>

ab1801a08369edeb2fe148b29ce13099e2ee024f
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-08-14 13:20:47 +0300

    feat: implement in JavaScript upload file from input event

--- resources/js/dropzone.js
+++ resources/js/dropzone.js
@@ -17,4 +17,24 @@ $(document).ready(function () {
         app.find('#drop-dir .drop-zone-prompt').text('Drop directory here or click to upload');
     });
 
+    app.on('click', '#drop-files .drop-zone', function () {
+        if (app.find('#file').length) {
+            app.find('#directory').remove();
+            app.find('#file').remove();
+            app.find('#upload-file').append(`<input type="file" id="file" name="files[]" multiple hidden />`);
+
+            app.on('change', '#file', function (e) {
+                const fileList = e.originalEvent.target.files;
+                $('.drop-zone').removeClass('border-black');
+                $('.drop-zone-prompt').removeClass('text-dark');
+                getListOfFile();
+                $.each(fileList, function (idx, file) {
+                    uploadFile(file);
+                });
+            });
+
+            $("#file").click();
+        }
+    });
+
 });

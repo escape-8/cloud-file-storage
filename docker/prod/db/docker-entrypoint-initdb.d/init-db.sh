a498aadc16a63bbda677748d3f9a0849b50d1fad
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-08-17 13:36:52 +0300

    feat: add create user in init-db.sh

--- docker/prod/db/docker-entrypoint-initdb.d/init-db.sh
+++ docker/prod/db/docker-entrypoint-initdb.d/init-db.sh
@@ -4,6 +4,7 @@ echo "** Creating default DB and users"
 
 mariadb -uroot -p$MYSQL_ROOT_PASSWORD --execute \
 "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
 
 echo "** Finished creating default DB and users"

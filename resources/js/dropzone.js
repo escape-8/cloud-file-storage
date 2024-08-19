7983c6ef2a270e3d80db31f57d61c7489aa24a7f
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-08-19 12:27:00 +0300

    feat: add filesize validation in js

--- resources/js/dropzone.js
+++ resources/js/dropzone.js
@@ -179,13 +179,20 @@ $(document).ready(function () {
     }
 
     function uploadFile(file) {
+
+        const id = Math.random().toString(36).substring(2);
+        const fileName = file.webkitRelativePath ? '/' + file.webkitRelativePath : file.name
+
+        if (checkMaxFileSize(file)) {
+            uploadResponse(fileName, id, file);
+            return;
+        }
+
         const formData = new FormData();
         formData.append('files[]', file);
         formData.append('_token', $('input[name=_token]').val());
         formData.append('path', new URL(app.find('#upload-file').attr('action')).searchParams.get('path'));
 
-        const id = Math.random().toString(36).substring(2);
-        const fileName = file.webkitRelativePath ? '/' + file.webkitRelativePath : file.name
 
         $.ajax({
             xhr: function() {
@@ -273,4 +280,44 @@ $(document).ready(function () {
         e.stopPropagation();
         e.preventDefault();
     });
+
+    function checkMaxFileSize(file) {
+        const KB_IN_MB = 1048576;
+        const maxUploadSizeMB = 20;
+        const MaxFileSizeInKilobytes = KB_IN_MB * maxUploadSizeMB;
+
+        return file.size > MaxFileSizeInKilobytes;
+    }
+
+    function uploadResponse (fileName, id, file) {
+        const KB_IN_MB = 1048576;
+        const filesizeMB = (file.size / KB_IN_MB).toFixed(2);
+        const fileItem = app.find('#upload-items').find(`#${id}`);
+
+        const html = `
+                            <div id="${id}" class="d-flex flex-row align-items-center bg-light w-100 rounded-3 p-3 mb-1" >
+                                <div class="d-flex align-self-center me-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="min-icon" fill="currentcolor" width="30px" viewBox="0 0 256 256"><g transform="translate(-13.993 -4638.241)"><g transform="translate(41.993 4638.241)"><g><path d="M137.775,74.688a10.333,10.333,0,0,1-10.366-10.243V0H25.916A25.839,25.839,0,0,0,0,25.607V230.393A25.839,25.839,0,0,0,25.916,256H177.084A25.839,25.839,0,0,0,203,230.393V74.688Z" fill="#295595"></path><path d="M83.343,74.614H149.3L73.787,0V65.158a10.275,10.275,0,0,0,9.556,9.456" transform="translate(53.697)" fill="#4a74b1"></path><path d="M118.8,112.994H34.133a2.47,2.47,0,0,0-2.481,2.451v9.806a2.473,2.473,0,0,0,2.481,2.451H118.8a2.477,2.477,0,0,0,2.481-2.451v-9.806a2.474,2.474,0,0,0-2.481-2.451" transform="translate(25.033 70.751)" fill="#fff"></path><path d="M118.8,94.244H34.133A2.47,2.47,0,0,0,31.652,96.7V106.5a2.474,2.474,0,0,0,2.481,2.451H118.8a2.477,2.477,0,0,0,2.481-2.451V96.7a2.474,2.474,0,0,0-2.481-2.452" transform="translate(25.033 58.859)" fill="#fff"></path><path d="M31.651,77.945v9.806A2.475,2.475,0,0,0,34.132,90.2H118.8a2.476,2.476,0,0,0,2.481-2.451V77.945a2.472,2.472,0,0,0-2.481-2.451H34.132a2.471,2.471,0,0,0-2.481,2.451" transform="translate(25.032 46.967)" fill="#fff"></path></g></g></g></svg>
+                                </div>
+                                <div class="d-flex flex-column w-100" style="max-width: 90%">
+                                    <div id="header-${id}" class="d-flex flex-row align-items-center justify-content-between mb-2">
+                                        <div class="d-flex w-75 flex-row align-items-center">
+                                            <div class="text-truncate w-50 me-2">${fileName}</div>
+                                            <span class="me-3 text-nowrap">${filesizeMB} MB</span>
+                                        </div>
+                                        <div class="d-flex flex-row">0 %</div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center">
+                                        <div class="progress w-100" style="height: 10px">
+                                            <div class="progress-bar" role="progressbar" style="width: 0" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+
+        fileItem.length ? fileItem.replaceWith(html) : app.find('#upload-items').append(html);
+
+        $(`#${id}`).after(`<span class="invalid-feedback d-inline-block" role="alert"><strong class="text-wrap">This file must not be greater than 20 megabytes</strong></span>`);
+    }
 });
